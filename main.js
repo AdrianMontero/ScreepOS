17ac2441dc218dd5d7184621376aa95b6eb3445b
Adrián Montero de Espinosa <adriianmontero@gmail.com>
2017-01-19 13:27:52 +0100

    Añadido core

---
+++ main.js
@@ -0,0 +1,98 @@
+//crear torre
+//Game.spawns['Spawn1'].room.createConstructionSite( 23, 22, STRUCTURE_TOWER );
+
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+
+module.exports.loop = function () {
+    //Defensa
+
+    //Torres
+    for(var name in Game.rooms) { //Buscamos en todas las habitaciones las torres y las guardamos en el array "targets"
+        var targets = Game.rooms[name].find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_TOWER); //Torres de cada habitacion
+                    }
+            });
+             if(targets.length > 0) { //Si targets no es 0
+                for(var i = 0; i < targets.length;i++){ //Por cada posicion del array targets
+                    if(targets[i]) {
+                        //Reparar torres
+                        var closestDamagedStructure = targets[i].pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: (structure) => structure.hits < structure.hitsMax
+                        });
+                        if(closestDamagedStructure) {
+                            targets[i].repair(closestDamagedStructure);
+                        }
+                
+                        //Atacar creeps ostiles
+                        var closestHostile = targets[i].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+                        if(closestHostile) {
+                            targets[i].attack(closestHostile);
+                        }
+                    }
+                }
+            }
+        console.log('prueba');
+    }
+
+    //Gestion de creeps
+
+    //Borrar creeps muertos
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+
+
+    //Recuento de creeps de cada tipo
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+    
+    var upgrader = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Upgraders: ' + upgrader.length);
+    
+    var builder = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    console.log('Builders: ' + builder.length);
+    
+    //Mostramos la energia de la sala.
+    for(var name in Game.rooms) {
+        console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
+    }
+    
+    
+    
+    //Revivimos a los creeps muertos hasta un minimo.
+    if(harvesters.length < 3) {
+        var newName = Game.spawns['sp1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
+    
+    if(upgrader.length < 3) {
+        var newName = Game.spawns['sp1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
+    }
+    
+    if(builder.length < 2) {
+        var newName = Game.spawns['sp1'].createCreep([WORK,CARRY,MOVE], undefined, {role: 'builder'});
+        console.log('Spawning new builder: ' + newName);
+    }
+
+    //Activamos los roles
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if(creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if(creep.memory.role == 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+}
---
+++ role.builder.js
@@ -0,0 +1,40 @@
+var roleBuilder = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+
+        if(creep.memory.building && creep.carry.energy == 0) {
+            creep.memory.building = false;
+            creep.say('harvesting');
+        }
+        if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.building = true;
+            creep.say('building');
+        }
+
+        if(creep.memory.building) {
+            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+            if(targets.length) {
+                if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            }
+        }
+        else {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(sources.length >= 2){ //Para no saturar los recursos
+                if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[1]);
+                }
+            }
+            else{
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+            }
+            
+        }
+    }
+};
+
+module.exports = roleBuilder;
